a23c8e8e5d42a775efd5a30ff3c0682afb0b407a
pedroLeaoBF <pedroleao@biofy.tech>
2025-02-21 16:08:50 -0300

    generate code from string function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PedroALeo/huffman-go
+
+go 1.23.1
---
+++ huffman-go.go
@@ -0,0 +1,111 @@
+package huffmango
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type node struct {
+	char        string
+	freq        int
+	left, right *node
+}
+
+func makeNodes(s string) []node {
+	var nodes []node
+	m := make(map[string]int)
+
+	for _, v := range s {
+		m[string(v)] += 1
+	}
+
+	fmt.Printf("%v\n", m)
+
+	for char, freq := range m {
+		nodes = append(nodes, node{char: char, freq: freq})
+	}
+
+	return nodes
+}
+
+func (n *node) isLeaf() bool {
+	return (n.left == nil) && (n.right == nil)
+}
+
+func printCode(root node, steps []int) {
+	if root.isLeaf() {
+		var code string
+		for _, s := range steps {
+			code += strconv.Itoa(s)
+		}
+		fmt.Printf("%v %v\n", root.char, code)
+		return
+	}
+
+	if root.left != nil {
+		leftSteps := steps
+		leftSteps = append(leftSteps, 0)
+		printCode(*root.left, leftSteps)
+	}
+
+	if root.right != nil {
+		rightSteps := steps
+		rightSteps = append(rightSteps, 1)
+		printCode(*root.right, rightSteps)
+	}
+}
+
+func removeAtIndex(nodes []node, index int) []node {
+	return append(nodes[:index], nodes[index+1:]...)
+}
+
+func newInternalNode(node1, node2 node) node {
+	return node{
+		char:  "Internal node",
+		freq:  node1.freq + node2.freq,
+		left:  &node1,
+		right: &node2,
+	}
+}
+
+func findLowestFreq(nodes []node) int {
+	lf := nodes[0].freq
+	ci := 0
+
+	for i, node := range nodes {
+		if node.freq < lf {
+			lf = node.freq
+			ci = i
+		}
+	}
+
+	return ci
+}
+
+func iteration(nodes []node) []node {
+	lfi1 := findLowestFreq(nodes)
+	lfn1 := nodes[lfi1]
+
+	nodes = removeAtIndex(nodes, lfi1)
+
+	lfi2 := findLowestFreq(nodes)
+	lfn2 := nodes[lfi2]
+
+	nodes = removeAtIndex(nodes, lfi2)
+
+	iNode := newInternalNode(lfn1, lfn2)
+
+	nodes = append(nodes, iNode)
+
+	return nodes
+}
+
+func CreateHuffmanCodeFromString(s string) {
+	nodes := makeNodes(s)
+
+	for len(nodes) > 1 {
+		iteration(nodes)
+	}
+
+	printCode(nodes[0], []int{})
+}
